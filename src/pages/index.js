0f4269a8283e279072fa046d8ef9db0038b10a02
Xinyi-Song <xinyi.song0825@gmail.com>
2024-02-12 15:44:41 -0500

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './qcomps/list_keys_id'
+import Menu from './qcomps/recipes.js'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <Menu/>
     </div>
   )
 }
